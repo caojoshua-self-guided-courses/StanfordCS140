bce558c4ee0a3748ab18e8062ce1480949845fa1
caojoshua <cao.joshua@yahoo.com>
2020-09-26 22:44:29 -0700

    file descriptor logic and open system call

--- pintos/userprog/syscall.c
+++ pintos/userprog/syscall.c
@@ -5,17 +5,90 @@
 #include "filesys/file.h"
 #include "filesys/filesys.h"
 #include "threads/interrupt.h"
+#include "threads/malloc.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
 #include "userprog/pagedir.h"
 
 static void syscall_handler (struct intr_frame *);
 
+/* Min fd. fd 0 and fd 1 are reserved for stdin and stdout 
+ * respectively. */
+static const int MIN_FD = 2;
+
+/* List of fd_entries, sorted by fd */
+static struct list fd_list;
+
+/* Elements of fd_list that map fd to files */
+struct fd_entry
+{
+  int fd;
+  struct file *file;
+  struct list_elem elem;
+};
+
+/* register syscall_handler for interrupts */
 void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  list_init (&fd_list);
 }
+
+/* Create and return a new file descriptor. */
+static int
+create_fd (const char *file_name)
+{
+  if (!file_name)
+    return -1;
+  struct file *file = filesys_open (file_name);
+  if (!file)
+    return -1;
+
+  int fd = MIN_FD;
+  struct fd_entry *fd_entry = malloc (sizeof (struct fd_entry));
+  fd_entry->file = file;
+
+  /* Iterate through the fd list until there is an open fd */
+  struct list_elem *e; 
+  for (e = list_begin (&fd_list); e != list_end (&fd_list); e = list_next (e))
+  {
+    struct fd_entry *fde = list_entry (e, struct fd_entry, elem); 
+    if (fd != fde->fd)
+    {
+      fd_entry->fd = fd;
+      e = list_next(e);
+      break;
+    }
+    ++fd;
+  }
+  fd_entry->fd = fd;
+  list_insert (e, &fd_entry->elem);
+  return fd_entry->fd;
+}
+
+/* Returns the file associated with the given fd */
+static struct file*
+get_file (int fd)
+{
+  struct list_elem *e;
+  for (e = list_begin (&fd_list); e != list_end (&fd_list); e = list_next (e))
+  {
+    struct fd_entry *fd_entry = list_entry (e, struct fd_entry, elem); 
+    if (fd_entry->fd == fd)
+      return fd_entry->file;
+  }
+  return NULL;
+}
+
+static bool
+is_valid_user_addr(const void *uaddr)
+{
+  return uaddr && is_user_vaddr(uaddr) &&
+    pagedir_get_page(thread_current()->pagedir, uaddr);
+}
+
+/********** SYSTEM CALLS **********/
 
 static void
 halt (void)
@@ -44,7 +117,7 @@ remove (const char *file)
 static int
 open (const char *file)
 {
-  return file_open;
+  return create_fd (file);
 }
 
 static int
@@ -56,13 +129,6 @@ write (int fd, const void *buffer, unsigned length)
   }
   // TODO: write to file
   return length;
-}
-
-static bool
-is_valid_user_addr(const void *uaddr)
-{
-  return uaddr && is_user_vaddr(uaddr) &&
-    pagedir_get_page(thread_current()->pagedir, uaddr);
 }
 
 static void
@@ -96,7 +162,7 @@ syscall_handler (struct intr_frame *f)
       f->eax = remove (*(const char **)(esp + 4));
       break;
     case SYS_OPEN:
-      /* f->eax = open (*(const char **)(esp + 4)); */
+      f->eax = open (*(const char **)(esp + 4));
       break;
     case SYS_WRITE:
       f->eax = write (*(int *)(esp + 4), (void *) *(int *)(esp + 8), 
